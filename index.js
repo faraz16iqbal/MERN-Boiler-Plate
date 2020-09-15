d5eebe8bbfabf53d30284bae4cafe4990098bacb
faraz16iqbal <53421337+faraz16iqbal@users.noreply.github.com>
2020-09-15 23:22:25 +0530

    Changes

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
+mongoose
+  .connect(
+    "mongodb+srv://faraz16iqbal:munifafa@cluster1.dhlct.mongodb.net/test1?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("DB Connected"))
+  .catch((err) => {
+    console.log(err);
+  });
 app.get("/", (req, res) => {
   res.send("hello world");
 });
