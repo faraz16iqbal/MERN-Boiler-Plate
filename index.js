9e77edf4c51c81da3624a2278f3d5284e7644bd9
faraz16iqbal <53421337+faraz16iqbal@users.noreply.github.com>
2020-09-18 00:34:38 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/users");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose
   .connect(
@@ -11,8 +18,17 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+  });
+});
+
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-
 app.listen(5000);
